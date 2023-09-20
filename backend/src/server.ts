0a0065505c002227a2d1f146e85d1f99f122d106
Vignesh <vignesh@pillarproject.io>
2023-09-20 16:37:53 +0530

    added cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,5 @@
 import fastify from 'fastify';
+import cors from '@fastify/cors'
 import config from './plugins/config.js';
 import routes from './routes/index.js';
 
@@ -14,6 +15,28 @@ const server = fastify({
     level: process.env.LOG_LEVEL,
   },
 });
+
+await server.register(cors, () => {
+  return (req: { headers: { origin: string; }; }, callback: (arg0: null, arg1: {
+      // This is NOT recommended for production as it enables reflection exploits
+      origin: boolean;
+    }) => void) => {
+    const corsOptions = {
+      // This is NOT recommended for production as it enables reflection exploits
+      origin: true
+    };
+
+    // do not include CORS headers for requests from localhost
+    if (/^localhost$/m.test(req.headers.origin) || /^127.0.0.1$/m.test(req.headers.origin) || /^0.0.0.0$/m.test(req.headers.origin)) {
+      corsOptions.origin = false
+      return
+    }
+
+    // callback expects two parameters: error and options
+    callback(null, corsOptions)
+  }
+})
+
 
 await server.register(config);
 await server.register(routes);
