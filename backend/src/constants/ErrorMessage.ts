d8c24cc3470a1e24307f0ff2a0c783390fc5e52c
vignesha22 <82584664+vignesha22@users.noreply.github.com>
2025-02-06 15:02:53 +0530

    Release 05.02.2025 (#178)
    * api endpoint changes for policy related apis (#173)
    
    * Error_Handling (#177)
    
    ---------
    
    Co-authored-by: Nikhil Kumar <48001923+nikhilkumar1612@users.noreply.github.com>

--- backend/src/constants/ErrorMessage.ts
+++ backend/src/constants/ErrorMessage.ts
@@ -51,7 +51,9 @@ export default {
     VP_ALREADY_DEPLOYED: 'Verifying paymaster already deployed',
     FAILED_TO_DEPLOY_VP: 'Failed to deploy verifying paymaster',
     FAILED_TO_ADD_STAKE: 'Failed to add stake',
-    INVALID_AMOUNT_TO_STAKE: 'Invalid amount to stake'
+    INVALID_AMOUNT_TO_STAKE: 'Invalid amount to stake',
+    MULTI_NOT_DEPLOYED: 'Token Paymaster not deployed on the current chainID: ',
+    COINGECKO_PRICE_NOT_FETCHED: 'Token price not updated, Pls retry.'
 }
 
 export function generateErrorMessage(template: string, values: { [key: string]: string | number }): string {
